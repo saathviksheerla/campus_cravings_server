168f22eea348b7ccfaa461db1812845959fc361d
Saathvik <saathvik.sheerla@gmail.com>
2025-04-09 20:36:09 +0530

    updated mongodb uri

--- app.js
+++ app.js
@@ -35,29 +35,29 @@ app.use(express.json());
 app.use(passport.initialize());
 
 // Connect to MongoDB Atlas
-// mongoose.connect(process.env.MONGODB_ATLAS_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   retryWrites: true,
-//   w: 'majority'
-// })
-// .then(() => {
-//   console.log('MongoDB Atlas connected successfully');
-//   // Log connection details (without sensitive info)
-//   const { host, name } = mongoose.connection;
-//   console.log(`Connected to database: ${name} at host: ${host}`);
-// })
-// .catch(err => {
-//   console.error('MongoDB Atlas connection error:', err);
-//   process.exit(1); // Exit if cannot connect to database
-// });
-
-mongoose.connect(process.env.MONGODB_LOCAL_URI, {
+mongoose.connect(process.env.MONGODB_ATLAS_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  retryWrites: true,
+  w: 'majority'
 })
-  .then(() => console.log('MongoDB connected successfully \n'))
-  .catch(err => console.error('MongoDB connection error:', err));
+.then(() => {
+  console.log('MongoDB Atlas connected successfully');
+  // Log connection details (without sensitive info)
+  const { host, name } = mongoose.connection;
+  console.log(`Connected to database: ${name} at host: ${host}`);
+})
+.catch(err => {
+  console.error('MongoDB Atlas connection error:', err);
+  process.exit(1); // Exit if cannot connect to database
+});
+
+// mongoose.connect(process.env.MONGODB_LOCAL_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+//   .then(() => console.log('MongoDB connected successfully \n'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/auth', authRoutes);
